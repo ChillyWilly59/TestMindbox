962e371ef15d492f5704c987f85bae274105c513
ivano <ivan.obraztcov@yahoo.com>
2023-11-25 14:44:02 +0400

    Добавьте файлы проекта.

---
+++ TestMindbox/GeometryTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace TestMindbox
+{
+    [TestFixture]
+    public class GeometryTests
+    {
+        [Test]
+        public void CircleAreaCalculation()
+        {
+            Circle circle = new(5);
+            Assert.AreEqual(Math.PI * 25, circle.GetArea(), 0.001);
+        }
+
+        [Test]
+        public void TriangleAreaCalculation()
+        {
+            Triangle triangle = new(3, 4, 5);
+            Assert.AreEqual(6, triangle.GetArea(), 0.001);
+        }
+
+        [Test]
+        public void RightAngledTriangleCheck()
+        {
+            Triangle triangle = new(3, 4, 5);
+            Assert.IsTrue(triangle.IsRightAngled());
+
+            Triangle nonRightAngledTriangle = new(3, 4, 6);
+            Assert.IsFalse(nonRightAngledTriangle.IsRightAngled());
+        }
+    }
+}
---
+++ TestMindbox/Square.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TestMindbox
+{
+    public interface IShape
+    {
+        double GetArea();
+    }
+
+    public class Circle(double radius) : IShape
+    {
+        public double Radius { get; } = radius;
+
+        public double GetArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+
+    public class Triangle(double sideA, double sideB, double sideC) : IShape
+    {
+        public double SideA { get; } = sideA;
+        public double SideB { get; } = sideB;
+        public double SideC { get; } = sideC;
+
+        public double GetArea()
+        {
+            double s = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+        }
+
+        public bool IsRightAngled()
+        {
+            double[] sides = [SideA, SideB, SideC];
+            Array.Sort(sides);
+            return Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) == Math.Pow(sides[2], 2);
+        }
+    }
+}
